e45fabc72465c6da1c304fb38410a665ac658cd5
User <User@SABADINNOT>
2024-09-05 14:13:45 -0300

    Adicionar arquivos de projeto.

---
+++ Rpgzao.cpp
@@ -0,0 +1,24 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RectangleShape shape(sf::Vector2f(100.0f, 100.0f));
+    shape.setFillColor(sf::Color::Red);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
